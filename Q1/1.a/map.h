c220c24210605e771871f3018cf7cbc598cba6bd
Msaleh <geshanisaleh@gamil.com>
2019-03-08 03:39:18 -0800

    second commit

---
+++ Q1/1.a/map.h
@@ -0,0 +1,44 @@
+#ifndef MAP_H
+#define MAP_H
+
+class Map
+{
+public:
+
+	Map () = default; //Default constructor
+	Map (int N);
+	~Map(); //Destructor
+
+	void showMap();
+	void findRoute();
+	void showRoute();
+
+	int n;
+	int** obstacle; //The random number assigned to each block
+	int** route; //The array which we use to display the select route with '*'
+	int* list; //The array which we use to calculate the total distance by storing the height of each block which is in the select route
+
+private:
+
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
